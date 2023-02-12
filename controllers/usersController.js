7cc074bb9e8dc7235bb2ea2669d0b863373bc3e3
araf821 <araf.ahmed200@gmail.com>
2023-02-12 17:58:37 -0500

    implemented update user functionality

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -45,18 +45,49 @@ const createNewUser = asyncHandler(async (req, res) => {
       .status(201)
       .json({ message: `New user, ${username}, has been created!` });
   } else {
-    res
-      .status(400)
-      .json({
-        message: "Invalid user data received. No new user was created.",
-      });
+    res.status(400).json({
+      message: "Invalid user data received. No new user was created.",
+    });
   }
 });
 
 // @desc Update A User
 // @route PATCH /users
 // @access Private
-const updateUser = asyncHandler(async (req, res) => {});
+const updateUser = asyncHandler(async (req, res) => {
+  const { id, username, roles, active, password } = req.body;
+
+  // Confirm Data
+  if (!id || !username || !roles.length || typeof active !== "boolean") {
+    return res.status(400).json({ message: "All fields are required." });
+  }
+
+  const user = await User.findById(id).exec();
+  // Check if the user exists
+  if (!user) {
+    return res.status(400).json({ message: "User not found." });
+  }
+
+  // Check for duplicate user if the update leads to duplicating usernames
+  const duplicate = await User.findOne({ username }).lean().exec();
+  // Allow updates to the origin, first user of the duplicates
+  if (duplicate && duplicate?._id.toString() !== id) {
+    return res.status(409).json({ message: "Duplicate username." });
+  }
+
+  user.username = username;
+  user.roles = roles;
+  user.active = active;
+
+  if (password) {
+    // Hash the new password by 10 salt rounds
+    user.password = await bcrypt.hash(password, 10);
+  }
+
+  const updatedUser = await user.save();
+
+  res.json({ message: `User ${updatedUser.username} updated successfully.` });
+});
 
 // @desc Delete A User
 // @route DELETE /users
