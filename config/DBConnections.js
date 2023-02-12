cb4474fc3b46742f1d7c70db7110a287e2c7aa4e
araf821 <araf.ahmed200@gmail.com>
2023-02-12 17:07:36 -0500

    set up mongoose connection and Note schema

---
+++ config/DBConnections.js
@@ -0,0 +1,11 @@
+const mongoDb = require("mongoose");
+
+const connection = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URI);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = connection;
